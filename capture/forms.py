e7009676f3fecb67b88d72ae4fb379964742b9ce
akella-vamsi <avkv133@gmail.com>
2021-05-04 11:35:43 +0530

    Latest

--- capture/forms.py
+++ capture/forms.py
@@ -1,6 +1,7 @@
 from django import forms
 
+
 class ImageForm(forms.Form):
     title = forms.CharField(max_length=50)
-    image = forms.FileField( )
+    image = forms.FileField()
     hidden_image_field=forms.CharField()
